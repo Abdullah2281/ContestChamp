92f18153de6505ac1cd08d4dd37f415244ee80b4
AnkitChoudhaary <ac225522@gmail.com>
2024-06-03 13:03:14 +0530

    intigrated profile section in backend

--- home/urls.py
+++ home/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
-    path('login',views.login,name='login'),
-    path('ratings',views.ratings,name='ratings'),
-    path('problems',views.problems,name='problems'),
-    path('contests',views.contests,name='contests'),
-    
+    path("", views.home, name="home"),
+    path("login", views.login, name="login"),
+    path("ratings", views.ratings, name="ratings"),
+    path("problems", views.problems, name="problems"),
+    path("contests", views.contests, name="contests"),
+    path("profile", views.profile, name="profile"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    return render(request, "home.html")
+
 
 def login(request):
-    return render(request,'login.html')
+    return render(request, "login.html")
+
 
 def ratings(request):
-    return render(request,'ratings.html')
+    return render(request, "ratings.html")
+
 
 def problems(request):
-    return render(request,'problems.html')
+    return render(request, "problems.html")
+
 
 def contests(request):
-    return render(request,'contests.html')
+    return render(request, "contests.html")
+
+
+def profile(request):
+    return render(request, "profile.html")
