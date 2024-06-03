21154596ad4842f1b88302fde64086fb7da3c668
AnkitChoudhaary <ac225522@gmail.com>
2024-06-03 10:03:17 +0530

    Integrated with django

---
+++ home/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('login',views.login,name='login'),
+    path('ratings',views.ratings,name='ratings'),
+    path('problems',views.problems,name='problems'),
+    path('contests',views.contests,name='contests'),
+    
+]
---
+++ home/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def ratings(request):
+    return render(request,'ratings.html')
+
+def problems(request):
+    return render(request,'problems.html')
+
+def contests(request):
+    return render(request,'contests.html')
