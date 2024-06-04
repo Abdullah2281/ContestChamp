ac1df2d65c8cc6364bbcee6edbd601edaeff57ec
AnkitChoudhaary <ac225522@gmail.com>
2024-06-04 22:50:56 +0530

    try d

--- build.sh
+++ build.sh
@@ -9,13 +9,18 @@ echo "Start Build"
 echo "Started Venv"
 # apt install python3.10-venv
 
+which python
+which pip
+
 python3 -m venv my_env
 source my_env/bin/activate
 
 python3 --version
 python3 -c "import sqlite3"
+
 # Install dependencies (Vercel will handle this during deployment)
 echo "Started my_env"
+
 pip3 install -r requirements.txt
 
 python3 manage.py runserver 3000
