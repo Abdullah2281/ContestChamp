d76fccf93c55ea20a09c73fe0da84dc5127f46d4
AnkitChoudhaary <ac225522@gmail.com>
2024-06-13 10:02:05 +0530

    versal Deploynment

--- build.sh
+++ build.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
-
-echo "Start Build"
-echo "Started Venv"
-
-
-source "./my_env/bin/activate"
+python3 --version
+python3 -m venv myenv
+source myenv/bin/activate
 which python3
+pip3 install -r requirements.txt
+sudo apt update
+sudo apt install apt
+sudo apt install curl wget
+wget http://ftp.us.debian.org/debian/pool/main/a/apt/apt_2.2.4_amd64.deb
+sudo dpkg -i apt_2.2.4_amd64.deb
+sudo apt --fix-broken install
+apt-get --version
 
-echo "Started my_env"
+sudo apt-get install libsqlite3-dev
+wget https://www.python.org/ftp/python/3.10.12/Python-3.10.12.tgz
+tar xzf Python-3.10.12.tgz
+cd Python-3.10.12
+./configure --enable-loadable-sqlite-extensions
+make
+sudo make install
 
-echo "Build completed successfully!"
+python3 manage.py runserver
