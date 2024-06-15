8476e4c2ed87856322c173d122956324dfa1a49d
Mayankj537 <mayankrocks5102@gamil.com>
2024-06-15 14:21:00 +0530

    Add friend functionality and update profile view

--- home/models.py
+++ home/models.py
@@ -14,20 +14,31 @@ from django.conf import settings
 #         return self.username
 
 # models.py
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class CustomUser(AbstractUser):
     rating = models.IntegerField(default=0)
-    university = models.CharField(max_length=30,default="")
+    university = models.CharField(max_length=30, default="")
+
     def __str__(self):
         return self.username
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    rating = models.IntegerField(default=0)  # Add the rating field here
-    university = models.CharField(max_length=30,default="")
+
     def __str__(self):
         return self.user.username
+
+
+class Friendship(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='friends', on_delete=models.CASCADE)
+    friend = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='friend_of', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} is friends with {self.friend.username}"
+
--- home/urls.py
+++ home/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("problems", views.problems, name="problems"),
     path("contests", views.contests, name="contests"),
     path("profile/<str:user_id>", views.profile, name="profile"),
+    path('add_friend/<str:user_id>/', views.add_friend, name='add_friend'),
     path("register", views.register, name="register"),
     path("logout/", views.logout_view, name="logout"),
     path("profile", views.user_view, name="empty_user"),
